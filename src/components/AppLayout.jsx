c39d75a9e8336925ac0878e311cf5df128bfc03f
oussama3422 <ossamarhayrhay2001@gmail.com>
2024-11-04 11:11:30 +0100

    new change

--- src/components/AppLayout.jsx
+++ src/components/AppLayout.jsx
@@ -1,5 +1,17 @@
-const AppLayout = ({ children }) => {
-  return <div style={{ background: "#1d1d1d", margin: "0px",width:"auto" }}>{children}</div>;
-};
+const AppLayout = ({ children }) => {  
+  return (  
+    <div  
+      style={{  
+        background: "#1d1d1d",  
+        margin: "0",  
+        padding: "0",  
+        minHeight: "100vh", // Ensure it covers the full height of the viewport  
+        overflow: "hidden", // Prevent horizontal overflow  
+      }}  
+    >  
+      {children}  
+    </div>  
+  );  
+};  
 
 export default AppLayout;
